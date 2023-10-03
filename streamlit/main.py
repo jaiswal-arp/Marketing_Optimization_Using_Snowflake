8988ed54c4a536403b8760241b2fb81c87104819
PranithaPoosa <poosa.p@northeastern.edu>
2023-10-03 11:13:31 -0400

    changes, added buttons to show and run query colored the updated query elements

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,7 +1,23 @@
 import streamlit as st
 import json
+from sqlalchemy import create_engine
+import pandas as pd
+from dotenv import load_dotenv
+import os
+import re
+
+load_dotenv()
 
 st.title("Assignment 1")
+
+sf_username = os.environ.get("SNOWFLAKE_USERNAME")
+sf_password = os.environ.get("SNOWFLAKE_PASSWORD")
+sf_account_identifier = os.environ.get("SNOWFLAKE_ACCOUNT_IDENTIFIER")
+sf_db = os.environ.get("SNOWFLAKE_DB")
+sf_schema = os.environ.get("SNOWFLAKE_SCHEMA")
+connection_string = f'snowflake://{sf_username}:{sf_password}@{sf_account_identifier}/{sf_db}/{sf_schema}'
+
+engine = create_engine(connection_string)
 
 data = {}
 with open("./queries.json") as f:
@@ -20,19 +36,31 @@ for k, v in names_queries.items():
             if v[1][i]["type"] == "dropdown":
                 var_value = st.selectbox(label=v[1][i]["name"], options= v[1][i]["values"])
             elif v[1][i]["type"] == "int":
-                # TODO: error handling, what if string is passed
-                var_value = int(st.text_input(label=v[1][i]["name"], value=0))
+                # TODO: error handling, what if string is passed - done
+                # TODO: put bounds to numbers
+                try:
+                    var_value = int(st.text_input(label=v[1][i]["name"], value=0))
+                except ValueError:
+                    print('Please enter a number!')
             elif v[1][i]["type"] == "string":
                 var_value = st.text_input(label=v[1][i]["name"], value='')
             else:
                 raise(ValueError(""))
             variables_list.append(var_value)
 
-if st.button('Submit'):
+button1 = st.button('Show Query')
+if st.session_state.get('button') != True:
+    st.session_state['button'] = button1
+
+if st.session_state['button'] == True:
     new_selected_query = selected_query.format(*variables_list)
-    st.write(new_selected_query)
-
-
+    colored_selected_query = re.sub(r'\{(\d+)\}', r':blue[{\1}]', selected_query)
+    st.write(colored_selected_query.format(*variables_list))
+    if st.button('Run'):
+        with engine.connect() as connection:
+            result = connection.execute(new_selected_query)
+            df = pd.DataFrame(result.fetchall(), columns=result.keys())
+            st.dataframe(df)
 
 
 
