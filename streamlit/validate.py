1c73b101de4effebb40cbf0b417b588b85d84bcb
jaiswal-arp <jaiswal.arp@northeastern.edu>
2023-10-05 17:06:33 -0400

    Added Error Handling for text and string inputs

--- streamlit/validate.py
+++ streamlit/validate.py
@@ -28,33 +28,37 @@ engine = create_engine(URL(
 connection = engine.connect()   
 try:
     # Streamlit app header
-    st.title('Interactive Snowflake Query Runner')
+    st.title('Samsung Marketing Insights Dashboard')
 
     data = {}
     with open("./sql.json") as f:
         data = json.load(f)    
-
+    
     names_queries = {i["name"]: {"query":i["query"], "variables":i["variables"]} for i in data["data"]}
 
-    option = st.selectbox('queries', names_queries.keys())
+    option = st.sidebar.selectbox('queries', names_queries.keys())
 
-    # st.write(names_queries[option])
 
     selected_query = names_queries[option]["query"]
 
     variables_list = []
     for var in names_queries[option]["variables"]:
         if var["type"] == "dropdown":
-            var_value = st.selectbox(label=var["name"], options= var["values"])
+            var_value = st.sidebar.selectbox(label=var["name"], options= var["values"])
         elif var["type"] == "int":
                 try:
-                    var_value = int(st.text_input(label=var["name"], value=0))
+                    var_value = int(st.sidebar.text_input(label=var["name"], value=0))
                 except ValueError:
-                    print('Please enter a number!')
+                    st.sidebar.write('Please enter a number!')
+                    continue
         elif var["type"] == "string":
-                var_value = st.text_input(label=var["name"], value='')
+                try:
+                    var_value = st.sidebar.text_input(label=var["name"], value='')
+                except ValueError:
+                    st.sidebar.write('Enter an proper value')
         else:
                 raise(ValueError(""))
+
         variables_list.append(var_value)
 
     st.write(variables_list)
@@ -62,7 +66,7 @@ try:
    # button1 = st.button('Show Query')
    # if st.session_state.get('button') != True:
     #    st.session_state['button'] = button1
-    if st.button('Run'):
+    if st.sidebar.button('Run'):
             new_selected_query = selected_query.format(*variables_list)
          #   colored_selected_query = re.sub(r'\{(\d+)\}', r':blue[{\1}]', selected_query)
           #  st.write(colored_selected_query.format(*variables_list))
@@ -74,12 +78,9 @@ try:
                 
     
             if option == "Store & Web Sales Quarterly Increment":
-                 st.write('AJ')
+                 st.write("Store & Web Sales Quarterly Increment Data")   
                  st.bar_chart(df)
-        
 
-
-    
     
 finally:
     connection.close()
