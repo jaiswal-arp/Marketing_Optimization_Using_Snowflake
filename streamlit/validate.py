62b5d6ff960ba4ae2693639dc83e0804b3c4de01
jaiswal-arp <jaiswal.arp@northeastern.edu>
2023-10-04 23:42:20 -0400

    Worked on the Query 1 UI and removed show query button

--- streamlit/validate.py
+++ streamlit/validate.py
@@ -2,6 +2,10 @@ from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
 import urllib.parse
 import streamlit as st
+import pandas as pd
+import numpy as np
+import json
+import re
 
 
 
@@ -26,57 +30,54 @@ try:
     # Streamlit app header
     st.title('Interactive Snowflake Query Runner')
 
-    # Dropdown menu for query selection
-    df = results = connection.execute('''with ss as (select ca_county,d_qoy, d_year,sum(ss_ext_sales_price) as store_sales
-     from store_sales,date_dim,customer_address
-     where ss_sold_date_sk = d_date_sk
-      and ss_addr_sk=ca_address_sk
-     group by ca_county,d_qoy, d_year),
-     ws as
-     (select ca_county,d_qoy, d_year,sum(ws_ext_sales_price) as web_sales
-     from web_sales,date_dim,customer_address
-     where ws_sold_date_sk = d_date_sk
-      and ws_bill_addr_sk=ca_address_sk
-     group by ca_county,d_qoy, d_year)
-     select 
-            ss1.ca_county
-           ,ss1.d_year
-           ,ws2.web_sales/ws1.web_sales web_q1_q2_increase
-           ,ss2.store_sales/ss1.store_sales store_q1_q2_increase
-           ,ws3.web_sales/ws2.web_sales web_q2_q3_increase
-           ,ss3.store_sales/ss2.store_sales store_q2_q3_increase
-     from
-            ss ss1
-           ,ss ss2
-           ,ss ss3
-           ,ws ws1
-           ,ws ws2
-           ,ws ws3
-     where
-        ss1.d_qoy = 1
-        and ss1.d_year = 1999
-        and ss1.ca_county = ss2.ca_county
-        and ss2.d_qoy = 2
-        and ss2.d_year = 1999
-     and ss2.ca_county = ss3.ca_county
-        and ss3.d_qoy = 3
-        and ss3.d_year = 1999
-        and ss1.ca_county = ws1.ca_county
-        and ws1.d_qoy = 1
-        and ws1.d_year = 1999
-        and ws1.ca_county = ws2.ca_county
-        and ws2.d_qoy = 2
-        and ws2.d_year = 1999
-        and ws1.ca_county = ws3.ca_county
-        and ws3.d_qoy = 3
-        and ws3.d_year =1999
-        and case when ws1.web_sales > 0 then ws2.web_sales/ws1.web_sales else null end 
-           > case when ss1.store_sales > 0 then ss2.store_sales/ss1.store_sales else null end
-        and case when ws2.web_sales > 0 then ws3.web_sales/ws2.web_sales else null end
-           > case when ss2.store_sales > 0 then ss3.store_sales/ss2.store_sales else null end
- order by web_q1_q2_increase;
-;''')
-    st.table(df)
+    data = {}
+    with open("./sql.json") as f:
+        data = json.load(f)    
+
+    names_queries = {i["name"]: {"query":i["query"], "variables":i["variables"]} for i in data["data"]}
+
+    option = st.selectbox('queries', names_queries.keys())
+
+    # st.write(names_queries[option])
+
+    selected_query = names_queries[option]["query"]
+
+    variables_list = []
+    for var in names_queries[option]["variables"]:
+        if var["type"] == "dropdown":
+            var_value = st.selectbox(label=var["name"], options= var["values"])
+        elif var["type"] == "int":
+                try:
+                    var_value = int(st.text_input(label=var["name"], value=0))
+                except ValueError:
+                    print('Please enter a number!')
+        elif var["type"] == "string":
+                var_value = st.text_input(label=var["name"], value='')
+        else:
+                raise(ValueError(""))
+        variables_list.append(var_value)
+
+    st.write(variables_list)
+
+   # button1 = st.button('Show Query')
+   # if st.session_state.get('button') != True:
+    #    st.session_state['button'] = button1
+    if st.button('Run'):
+            new_selected_query = selected_query.format(*variables_list)
+         #   colored_selected_query = re.sub(r'\{(\d+)\}', r':blue[{\1}]', selected_query)
+          #  st.write(colored_selected_query.format(*variables_list))
+        
+            with engine.connect() as connection:
+                result = connection.execute(new_selected_query)
+                df = pd.DataFrame(result.fetchall(), columns=result.keys())
+                st.dataframe(df)
+
+
+
+    
+   
+    
+    
 finally:
     connection.close()
     engine.dispose()
