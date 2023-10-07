3947600f6ef05f057a28bb4df1b97f5c9579fd22
PranithaPoosa <114799068+PranithaPoosa@users.noreply.github.com>
2023-10-06 20:30:06 -0400

    changes

--- streamlit/validate.py
+++ streamlit/validate.py
@@ -1,30 +1,21 @@
-from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
-import urllib.parse
 import streamlit as st
 import pandas as pd
-import numpy as np
+import os
 import json
-import re
+from dotenv import load_dotenv
 
+load_dotenv()
 
+sf_username = os.environ.get("SNOWFLAKE_USERNAME")
+sf_password = os.environ.get("SNOWFLAKE_PASSWORD")
+sf_account_identifier = os.environ.get("SNOWFLAKE_ACCOUNT_IDENTIFIER")
+sf_db = os.environ.get("SNOWFLAKE_DB")
+sf_schema = os.environ.get("SNOWFLAKE_SCHEMA")
+connection_string = f'snowflake://{sf_username}:{sf_password}@{sf_account_identifier}/{sf_db}/{sf_schema}'
 
-quoted_password = urllib.parse.quote("Hellrock@079")
+engine = create_engine(connection_string)
 
-url = f'snowflake://ajaiswal95:{quoted_password}@qefqbdp-ryb09972/SNOWFLAKE_SAMPLE_DATA/TPCDS_SF10TCL?COMPUTE_WH&ACCOUNTADMIN'
-
-engine = create_engine(url)
-
-engine = create_engine(URL(
-        user='ajaiswal95',
-        password=quoted_password,
-        account='qefqbdp-ryb09972',
-        database = 'SNOWFLAKE_SAMPLE_DATA',
-        schema = 'TPCDS_SF10TCL',
-        warehouse = 'COMPUTE_WH',
-        role = 'ACCOUNTADMIN',
-    )
-)
 connection = engine.connect()   
 try:
     # Streamlit app header
@@ -33,10 +24,10 @@ try:
     st.sidebar.title("Adjust Parameters")
 
     data = {}
-    with open("./sql.json") as f:
+    with open("streamlit/sql.json") as f:
         data = json.load(f)    
     
-    names_queries = {i["name"]: {"query":i["query"], "variables":i["variables"]} for i in data["data"]}
+    names_queries = {i["name"]: {"query": i["query"], "variables": i["variables"]} for i in data["data"]}
 
     option = st.sidebar.selectbox('queries', names_queries.keys())
 
@@ -56,7 +47,7 @@ try:
            # if var["name"] == 'Manufacturer ID' and var_value < 3 :
             #     st.sidebar.write("Only enter 3 digits")
 
-
+            
         elif var["type"] == "string":
                 try:
                     var_value = st.sidebar.text_input(label=var["name"], value='')
@@ -67,29 +58,25 @@ try:
 
         variables_list.append(var_value)
 
-   # st.write(variables_list)
-
-   # button1 = st.button('Show Query')
-   # if st.session_state.get('button') != True:
-    #    st.session_state['button'] = button1
     if st.sidebar.button('Run'):
             new_selected_query = selected_query.format(*variables_list)
-         #   colored_selected_query = re.sub(r'\{(\d+)\}', r':blue[{\1}]', selected_query)
-          #  st.write(colored_selected_query.format(*variables_list))
 
             with engine.connect() as connection:
+                # try:
                 result = connection.execute(new_selected_query)
                 df = pd.DataFrame(result.fetchall(), columns=result.keys())
                 st.dataframe(df)
+                # except:
+                #     st.write("Unexpected Input!")
 
 
            # if option == "Store & Web Sales Quarterly Increment":
-           #      st.write("Store & Web Sales Quarterly Increment Data")
+           #      st.write("Store & Web Sales Quarterly Increment Data")   
            #      st.bar_chart(df)
            # elif option == "Manufacturer Sales Analysis":
            #      st.write("Manufacturer Sales Analysis Data")
-
-
+                 
+         
 
     
 finally:
